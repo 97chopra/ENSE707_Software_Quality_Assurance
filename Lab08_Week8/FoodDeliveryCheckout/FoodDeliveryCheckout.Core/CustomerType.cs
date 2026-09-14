4639b4f8add1c8933d57ab099ba7e86c15d589de
Aarti <artichopra199@email.com>
2026-09-15 09:47:52 +1200

    Lab 8: Set up FoodDeliveryCheckout solution with Core implementation (OrderItem, CheckoutCalculator, enums)

---
+++ Lab08_Week8/FoodDeliveryCheckout/FoodDeliveryCheckout.Core/CustomerType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodDeliveryCheckout.Core;
+
+public enum CustomerType
+{
+    Regular,
+    Premium,
+    Student
+}
